213237f75e63085dcfa1b57c80219d6e7d7b898a
Steve Lally <relax1289@gmail.com>
2018-06-21 13:21:43 -0400

    Update Wherefore-art-thou.js
    Added comments

--- Wherefore-art-thou.js
+++ Wherefore-art-thou.js
@@ -6,16 +6,21 @@ function whatIsInAName(collection, source) {
   // What's in a name?
   var arr = [];
   // Only change code below this line
+  
+  // Function to check if a given key, value pair exist in given object
   let keyValueCheck = function(obj, key, value){
     if(obj.hasOwnProperty(key) && obj[key] === value){
       return true;
     }
   }
   
+  // Iterate through "collection" (the input array of objects)
   for (let i = 0; i < collection.length; i++){
     let match = false;
     
+    // Iterate through source object to compare with "collection" array of objects
     for(let item in source){
+      // If the current key value pair exists in the object, move onto the next, otherwise break out of the loop with "match" as false
       if (keyValueCheck(collection[i], item, source[item])){
         match = true;
       } else {
@@ -24,10 +29,12 @@ function whatIsInAName(collection, source) {
       }
     }
 
+    // push the current "collection" object to the "arr" array only if match came out of loop as true
     if (match === true) {
       arr.push(collection[i]);
     }
   }
+  
   // Only change code above this line
   return arr;
 }
